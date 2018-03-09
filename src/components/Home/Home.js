7916d6a48d319a3de59ad1d2002e98f11e0c597e
Taras Bobak <tbobak@tbobak-wf.local>
2018-03-09 07:30:04 +0200

    Added bootstrap grid styles into the project

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './Home.scss';
 
 function Home() {
-  return <h1>This is home page</h1>;
+  return (
+    <div>
+      <h1>This is home page</h1>
+      <h3>Bootstrap grid system</h3>
+      <div className="row">
+        <div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
+        <div className="col-6 col-sm-6">.col-6 .col-sm-6</div>
+        <div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
