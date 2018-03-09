fd5bcc2fca94edac565066cfb73806dafe552641
Taras Bobak <tbobak@tbobak-wf.hw.griddynamics.net>
2018-03-09 13:50:16 +0200

    Fixed eslint errors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import logo from '../../logo.svg';
 import Home from '../Home/Home';
 import Feedback from '../Feedback/Feedback';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React-POC</h1>
-          <Link to="/" className="App-header-link">Home</Link>
-          <Link to="/feedback" className="App-header-link">Feedback</Link>
-        </header>
-        <main>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/feedback" component={Feedback} />
-        </main>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React-POC</h1>
+        <Link to="/" className="App-header-link">Home</Link>
+        <Link to="/feedback" className="App-header-link">Feedback</Link>
+      </header>
+      <main>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/feedback" component={Feedback} />
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App/>
-  </BrowserRouter>, document.getElementById('root'));
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
