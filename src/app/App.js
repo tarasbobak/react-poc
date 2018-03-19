d9e6b7cf63176187d7541e0fd8a5d8b213892077
Dmitry Denysiuk <denisiuk@gmail.com>
2018-03-19 16:53:42 +0200

    404 page has been added.

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Link } from 'react-router-dom';
-import logo from '../logo.svg';
+import Switch from 'react-router-dom/Switch';
+import logo from '../assets/logo.svg';
 import Home from './Home/Home';
 import Feedback from './Feedback/Feedback';
+import NotFound from './NotFound';
 import dependencies, { registerDependencies } from './dependencies';
 import styles from './App.scss';
 
@@ -21,14 +23,17 @@ class App extends Component {
         <header className={styles.header}>
           <img src={logo} className={styles.logo} alt="logo" />
           <h1 className={styles.title}>Welcome to React-POC</h1>
-          <Link to="/" className={styles['header-link']}>Home</Link>
-          <Link to="/feedback" className={styles['header-link']}>
-            Feedback
-          </Link>
         </header>
-        <main>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/feedback" component={Feedback} />
+        <nav className={styles.nav}>
+          <Link to="/" className={styles['header-link']}>Home</Link>
+          <Link to="/feedback" className={styles['header-link']}>Feedback</Link>
+        </nav>
+        <main className={styles.main}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/feedback" component={Feedback} />
+            <Route path="*" component={NotFound} />
+          </Switch>
         </main>
       </div>
     );
